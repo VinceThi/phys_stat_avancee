4da34a76186ba4733c139391d74163f1b70e9ebf
xavier-rp <34655646+xavier-rp@users.noreply.github.com>
2018-02-17 16:26:28 -0500

    Update diagramme_hK.py

--- Dev1_phy_stat_av/diagramme_hK.py
+++ Dev1_phy_stat_av/diagramme_hK.py
@@ -13,7 +13,6 @@ def h_prime(h, k):
 def k_prime(h, k):
     return (1/4)*(np.log(np.cosh(2*k+h))+np.log(np.cosh(-2*k+h)) - np.log((np.cosh(h)*np.cosh(h))))
 
-# J'essaie une modification avec Github
 
 if __name__ == '__main__':
 
@@ -22,78 +21,45 @@ if __name__ == '__main__':
     k = np.arange(0, 1000)
     i = 0
     x = 200
-    h = np.arange(0,5)
-    j = 0
-    for h in np.arange(0,20):
-        h_init = h
-        for x in [5,10]:
-            h = h_init
-            i = 0
-            while i < 100:
-                x_prim = k_prime(h, x)
-                b_prim = h_prime(h , x)
-                print(x_prim, b_prim)
-            
-                #plt.plot([x , x_prim], [x_prim, x_prim], 'b')
-                #plt.plot([x_prim, x_prim],[x_prim, k_prime(h, x_prim)], 'b')
-                plt.plot([h, b_prim], [x, x_prim])
-            
-                x = x_prim
-                h = b_prim
-                i += 1
-    plt.show()
-    z = np.linspace(0, 20,1000)
-    plt.plot(z, z)
-    plt.plot(z, k_prime(0, z))
-    plt.show()
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
 
-    h = np.linspace(0,10)
-    k = np.linspace(0,10)
-    h ,k = np.meshgrid(h, k)
-    z = k_prime(h,k)
-    z2 = k
+    #for h in np.linspace(0,10, 20):
+    #    hinit = h
+    #    for x  in np.arange(1,10):
+    #        h = hinit
 
-    surf = ax.plot_surface(h, k, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    #        i = 0
+    #        while i < 100:
+    #            x_prim = k_prime(h, x)
+    #            b_prim = h_prime(h , x)
+    #            print(x_prim, b_prim)
 
-    ax.set_zlim(0, 10)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-    surf = ax.plot_surface(h, k, z2, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    ax.set_xlabel('h Label')
-    ax.set_ylabel('K Label')
-    ax.set_zlabel('k_prime Label')
+    #            #plt.plot([x , x_prim], [x_prim, x_prim], 'b')
+    #            #plt.plot([x_prim, x_prim],[x_prim, k_prime(h, x_prim)], 'b')
+    #            #plt.plot([h, b_prim], [x, x_prim])
 
-    plt.show()
+    #            plt.quiver([h],[x],[( b_prim - h)], [( x_prim- x)], angles='xy', scale_units='xy', scale=1, width=0.005 )
 
-    plt.plot(k, k_prime(h, k))
-    plt.plot(k,k)
-    print(k_prime(h,k) - k)
-    plt.show()
+    #            x = x_prim
+    #            h = b_prim
+    #            if abs(x) < 0.0001 :
+    #                break
+    #            i += 1
 
-    ##h = np.linspace(0,1)
-    #k = 0.5
-
-    #i = 0
-    #x = .5
-    #for k in np.arange(0,10):
-    #    i = 0
-    #    x = 0.0001
-    #    while i < 50:
-    #        x_prim = h_prime(x, k)
-    #        print(x_prim)
-
-    #        #plt.plot([x, x_prim], [x_prim, x_prim], 'b')
-    #        #plt.plot([x_prim, x_prim], [x_prim, h_prime(x_prim, k)], 'b')
-    #        plt.plot([x, x_prim], [k, k])
-
-    #        x = x_prim
-    #        i += 1
-
-    ##plt.plot(h, h_prime(h, k))
-    ##plt.plot(h, h)
+    #plt.xlim(0, 20)
+    ##plt.ylim(0, 10)
     #plt.show()
+
+    hvalues = np.linspace(0, 3, 15)
+    kvalues = np.linspace(0, 2, 10)
+    for h in hvalues:
+        for k in kvalues:
+            k_prim = k_prime(h, k)
+            h_prim = h_prime(h, k)
+
+            plt.quiver([h], [k], [(h_prim - h)], [(k_prim - k)], angles='xy', scale_units='xy', scale=3.5, width=0.005)
+            #plt.quiver([h], [k], [(h_prim - h)], [(k_prim - k)], units = 'width')
+
+    plt.xlim(0, 2)
+    # plt.ylim(0, 10)
+    plt.show()
